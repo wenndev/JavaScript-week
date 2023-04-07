8a7348925a68c0f3207101109004e0d3bcaf06b7
Wendel Oliveira <72776221+wenndev@users.noreply.github.com>
2023-04-07 13:17:49 -0300

    Update reduce.js

--- boilerplate/reduce.js
+++ boilerplate/reduce.js
@@ -5,10 +5,22 @@
     o array á um valor.
 */ 
 
+/*
+função executada, para cada item do array.
+na primeira vez que a função for executada, o accumulator vai receber o 0, e item vai receber o primeiro item do array (numero 1), 
+e fará a soma 1 + 0 = 1;
+na segunda execução o accumulator armazena o valor que a funçao retornou na execução passada que foi 1, e o item recebe o segundo item
+do array (numero 2) e fará a soma 1 + 2 = 3;
+e na terceira execução o accumulator armazena o valor que a função retornou na execução passada que foi 3, e o item recebe o 
+terceiro item do array(numero 3) e fará a soma 3 + 3 = 6;
+
+*/
 
 const numbers = [1,2,3];
 const sumResult = numbers.reduce((accumulator,item)=>accumulator + item,0);
 console.log(sumResult);
+
+
 
 
 /* 
